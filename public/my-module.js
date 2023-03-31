a30ceae3283efdc5668cc8c4d4633e630003f973
aadritChauhan <114449390+aadritChauhan@users.noreply.github.com>
2023-03-30 23:38:06 -0400

    updated

--- public/my-module.js
+++ public/my-module.js
@@ -1,6 +1,6 @@
-import { theShire, buckland } from './another-module';
+import { theShire, buckland } from './another-module.js';
 
-const statement = () => `${theShire.name} "was founded" ${buckland.founding - theShire.founding} "years before" ${buckland.name}.`;
+const statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
 
 export {
 	statement,
